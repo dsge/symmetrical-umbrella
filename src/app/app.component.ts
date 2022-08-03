94e53417751409b951b521c323e43256e0e92c45
dsge3 <gerx03@gmail.com>
2022-08-03 18:51:48 +0200

    implement basic currency exchange functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'symmetrical-umbrella';
+export class AppComponent implements OnInit{
+
+  form = new FormGroup({
+      currencyFrom: new FormControl('', [Validators.required]),
+      currencyTo: new FormControl('', [Validators.required]),
+      currencyAmount: new FormControl('', [Validators.required]),
+  });
+
+  results: {
+    convertedAmount:number,
+    directConversionEstimate: number|null,
+    wasConvertedThroughEur:boolean
+  } | null = null;
+
+  constructor(protected http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  private submitHttpCall() {
+    return this.http.get<any>('http://localhost:3000?' +
+      'from=' + this.form.value.currencyFrom +
+      '&to=' + this.form.value.currencyTo +
+      '&amount=' + this.form.value.currencyAmount
+    );
+  }
+
+  onSubmit(): void {
+    this.form.disable();
+    this.results = null;
+
+    const sub = this.submitHttpCall().subscribe({
+      next: (value) => {
+        this.results = value;
+        sub.unsubscribe();
+        this.form.enable();
+      },
+      error: (err) => {
+        sub.unsubscribe();
+        this.form.enable();
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
